8009c532a67cd8a2b59d9550f5ac002924b36d30
Brandon Martinez <1brandonmartinez1@gmail.com>
2022-06-16 11:37:44 -0500

    Adjusts LineRenderer width and gives it color

---
+++ Bigger_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,113 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Celestial::Awake()
+extern void Celestial_Awake_m66A6A4430D1F9ABB3978ABAD1628A60D3139DFBE (void);
+// 0x00000002 System.Void Celestial::UpdateVelocity(UnityEngine.Vector3,System.Single)
+extern void Celestial_UpdateVelocity_m48A3A93D84B57C1F997562FEF9FD25738011A375 (void);
+// 0x00000003 System.Void Celestial::UpdatePosition(System.Single)
+extern void Celestial_UpdatePosition_mA3A5AD515BBE1D9C8620711BE23865862DB17C81 (void);
+// 0x00000004 UnityEngine.Rigidbody Celestial::get_Rigidbody()
+extern void Celestial_get_Rigidbody_m08B4FF27A0D853E1CB4170E54164CF036583210F (void);
+// 0x00000005 UnityEngine.Vector3 Celestial::get_Position()
+extern void Celestial_get_Position_mFADA44C9906D4BA6E74A30044901A3E6A9A73686 (void);
+// 0x00000006 System.Void Celestial::.ctor()
+extern void Celestial__ctor_m8ECA39B5C6E1A0FA9BECC31F02B9D456C8DB66B4 (void);
+// 0x00000007 System.Void NewTemp::Start()
+extern void NewTemp_Start_m01193041EAA52C2D7537EF0AE874A265A5342D3E (void);
+// 0x00000008 System.Void NewTemp::Update()
+extern void NewTemp_Update_m874E74ADCA4F224E4111944F495D923ADDC8F879 (void);
+// 0x00000009 System.Void NewTemp::FixedUpdate()
+extern void NewTemp_FixedUpdate_mD825BE65E32A9EB5624F0E2F211633882D4E34EC (void);
+// 0x0000000A System.Void NewTemp::Gravity()
+extern void NewTemp_Gravity_mE2C61130DCFDB3F8899865F31AD953DF86228D7A (void);
+// 0x0000000B System.Void NewTemp::InitialVelocity()
+extern void NewTemp_InitialVelocity_m0E7CE8A08AA2E8591AECF47191188634C9580FB1 (void);
+// 0x0000000C System.Void NewTemp::.ctor()
+extern void NewTemp__ctor_m6A82719608CDBC8653775300FDA7C5258EDFF1C2 (void);
+// 0x0000000D System.Void UpdateCelestials::Start()
+extern void UpdateCelestials_Start_m2CECBBB84BBBCEBE531A8CF91DC1CAA4663944B2 (void);
+// 0x0000000E System.Void UpdateCelestials::FixedUpdate()
+extern void UpdateCelestials_FixedUpdate_mFD001F4543324D70F8C79987A377F1A5EB1EE8EC (void);
+// 0x0000000F UnityEngine.Vector3 UpdateCelestials::CalculateAcceleration(UnityEngine.Vector3,Celestial)
+extern void UpdateCelestials_CalculateAcceleration_mB79C5699F16A1707384474484ADC01A608181941 (void);
+// 0x00000010 Celestial[] UpdateCelestials::get_Celestials()
+extern void UpdateCelestials_get_Celestials_mC821C09F5C23CCE795B5E36404EDE03A419F1AA5 (void);
+// 0x00000011 UpdateCelestials UpdateCelestials::get_Instance()
+extern void UpdateCelestials_get_Instance_m440EC9613415ED72834A6F584D3AB5F4E32F1526 (void);
+// 0x00000012 System.Void UpdateCelestials::.ctor()
+extern void UpdateCelestials__ctor_m68915F270470899DDE7BF08CE7C20EE410099247 (void);
+static Il2CppMethodPointer s_methodPointers[18] = 
+{
+	Celestial_Awake_m66A6A4430D1F9ABB3978ABAD1628A60D3139DFBE,
+	Celestial_UpdateVelocity_m48A3A93D84B57C1F997562FEF9FD25738011A375,
+	Celestial_UpdatePosition_mA3A5AD515BBE1D9C8620711BE23865862DB17C81,
+	Celestial_get_Rigidbody_m08B4FF27A0D853E1CB4170E54164CF036583210F,
+	Celestial_get_Position_mFADA44C9906D4BA6E74A30044901A3E6A9A73686,
+	Celestial__ctor_m8ECA39B5C6E1A0FA9BECC31F02B9D456C8DB66B4,
+	NewTemp_Start_m01193041EAA52C2D7537EF0AE874A265A5342D3E,
+	NewTemp_Update_m874E74ADCA4F224E4111944F495D923ADDC8F879,
+	NewTemp_FixedUpdate_mD825BE65E32A9EB5624F0E2F211633882D4E34EC,
+	NewTemp_Gravity_mE2C61130DCFDB3F8899865F31AD953DF86228D7A,
+	NewTemp_InitialVelocity_m0E7CE8A08AA2E8591AECF47191188634C9580FB1,
+	NewTemp__ctor_m6A82719608CDBC8653775300FDA7C5258EDFF1C2,
+	UpdateCelestials_Start_m2CECBBB84BBBCEBE531A8CF91DC1CAA4663944B2,
+	UpdateCelestials_FixedUpdate_mFD001F4543324D70F8C79987A377F1A5EB1EE8EC,
+	UpdateCelestials_CalculateAcceleration_mB79C5699F16A1707384474484ADC01A608181941,
+	UpdateCelestials_get_Celestials_mC821C09F5C23CCE795B5E36404EDE03A419F1AA5,
+	UpdateCelestials_get_Instance_m440EC9613415ED72834A6F584D3AB5F4E32F1526,
+	UpdateCelestials__ctor_m68915F270470899DDE7BF08CE7C20EE410099247,
+};
+static const int32_t s_InvokerIndices[18] = 
+{
+	4623,
+	2171,
+	3728,
+	4528,
+	4615,
+	4623,
+	4623,
+	4623,
+	4623,
+	4623,
+	4623,
+	4623,
+	4623,
+	4623,
+	6303,
+	7015,
+	7015,
+	4623,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	18,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
